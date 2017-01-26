3b448b2262bfb084a7c96c351f738f69122af734
bubbabuford <bajobozic@gmail.com>
2017-01-27 00:36:40 +0100

    forgeted file app module

--- app/src/main/java/com/example/bajob/movieshatch/DependencyInjection/ApplicationModule.java
+++ app/src/main/java/com/example/bajob/movieshatch/DependencyInjection/ApplicationModule.java
@@ -1,8 +1,28 @@
 package com.example.bajob.movieshatch.DependencyInjection;
 
+import android.app.Application;
+
+import com.example.bajob.movieshatch.MoviesHatchApp;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * Created by bajob on 1/26/2017.
  */
-
+@Module
 public class ApplicationModule {
+    Application application;
+
+    public ApplicationModule(Application application) {
+        this.application = application;
+    }
+
+    @Singleton
+    @Provides
+    public Application getApplication() {
+        return application;
+    }
 }
